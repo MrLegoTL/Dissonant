f1f99f03a7f47f552c6d58537161ed188ed0bd43
Martin <martingarciacurto@gmail.com>
2024-05-29 21:32:46 +0200

    Scripts para logros y Inicio de MainMenu

---
+++ Assets/Scripts/Data/AchievementManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//para realizar consultas a arrays
+using System.Linq;
+//para hacer uso de los actions
+using System;
+
+public class AchievementManager : MonoBehaviour
+{
+    //Action de desbloqueo de logro que informara del nombre del logro y de su imagen
+    public static Action<string, string> OnAchievementUnlock;
+
+    private void OnEnable()
+    {
+        //suscripcion al metodo de muerte del enemigo mediante el uso de un método anónimo
+        //tened en cuenta que este metodo no podra ser DESUSCRITO por lo que debera utilizarse 
+        //en clases que sean persistentes en nuestro proyecto
+    }
+
+    /// <summary>
+    /// Incrementa una estadistica y verifica sus logros asociados
+    /// </summary>
+    /// <param name="code"></param>
+    /// <param name="amount"></param>
+    public void IncreaseStatAndCheckAchiviement(string code, int amount)
+    {
+        //recuperamos la estadistica solicitada, mediante el uso de Linq
+        //haciendo una cosulta para recuperar aquella que coincida en el codigo
+        Stat stat = DataManager.instance.data.statistics.Where(s => s.code == code).FirstOrDefault();
+
+        //si no existe la estadistica solicitada, no hacemos nada
+        if (stat == null) return;
+        //incrementamos la estadistica con el valor especificado
+        stat.value += amount;
+
+        //recorremos todos los logros que dependen de esta estadistica, que no hayan sido desbloqueada previamente
+        foreach (Achievement achievement in
+                 DataManager.instance.data.achievements.Where(a=> a.statCode == code && !a.unlocked).AsEnumerable())
+        {
+            //si con el valor desbloqueamos el logro, lo marcaremos como completado
+            //e invocaremos el evento
+            if(stat.value >= achievement.targetAmount)
+            {
+                achievement.unlocked = true;
+                //lanzamos el action con la informacion del logro desbloqueado
+                OnAchievementUnlock?.Invoke(achievement.name, achievement.imageName);
+                //adicionalmente pedimos al datamanager que guarde al desbloquear un logro
+                DataManager.instance.Save();
+            }
+        }
+    }
+   
+}
--- Assets/Scripts/Data/Data.cs
+++ Assets/Scripts/Data/Data.cs
@@ -11,4 +11,9 @@ public class Data
     public string entrancePoint;
     //array con el estado de todas las condiciones
     public Condition[] allCondition;
+
+    //listado de estadisticas disponibles
+    public Stat[] statistics;
+    //listado de logro disponibles
+    public Achievement[] achievements;
 }
